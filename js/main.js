965e10cc3adbe97ed146f82fce8f3df1b4323e50
Jason Natividad <jainatividad@yahoo.com>
2017-04-09 20:16:11 -0700

    Add test index files, add clear button functionality, and load bills in new tabs.  Project finished

--- js/main.js
+++ js/main.js
@@ -7,31 +7,52 @@
 
 
     var loadBtn = document.getElementById("load-button");
+    var mainText = document.querySelector("#main-text");
+    var clearBtn = document.getElementById("clear-button");
 
+    //var linkBase =
+
+
+    // Event listeners
     loadBtn.addEventListener("click", loadBills);
-
-
-
+    clearBtn.addEventListener("click", clearTextArea);
+    //
+    console.log(clearBtn);
 
     function loadBills() {
 
-        var mainText = document.querySelector("#main-text").value;
 
         var expression = /([G{1}][C{1}]\d{7})/g;
 
         //mainText = mainText.replace(/[\n\r]+/g, '');
 
-
+        var mainText = document.querySelector("#main-text").value;
 
         //[[G{1}][C{1}]\d{7}
 
 
-        var listOfBills = mainText.match(expression);;
+        var listOfBills = mainText.match(expression);
 
         //var listOfBills = mainText.split(expression);
 
         console.log(listOfBills);
 
+        listOfBills.forEach(function(index) {
+
+            window.open(index + ".html");
+        });
+
+        //window.open(listOfBills[0]+ ".html");
+
+
+    }
+
+
+
+    function clearTextArea() {
+        var mainText = document.querySelector("#main-text");
+
+        mainText.value = "";
 
     }
 
